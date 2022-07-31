8e8e8302f26f633886ff5f7666439b224dd45412
Omri Ruvio <omriruvio@gmail.com>
2022-07-31 09:40:31 +0300

    deploy: update API url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://39ad-176-228-28-109.eu.ngrok.io';
+// export const BASE_URL = 'https://39ad-176-228-28-109.eu.ngrok.io';
+export const BASE_URL = 'https://hydroponicsil.herokuapp.com';
